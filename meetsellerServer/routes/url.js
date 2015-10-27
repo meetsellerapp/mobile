cbb557a824fc24f6145219a4433086a9bc75769f
tuancat <tuan.catnghiemhieu@gmail.com>
2015-10-27 08:51:52 +0700

    - get URL services

--- meetsellerServer/routes/url.js
+++ meetsellerServer/routes/url.js
@@ -2,12 +2,14 @@
  * GET users listing.
  */
 var urlService = require("../public/services/url");
-var Q = require("q");
-
+var Firebase = require("firebase");
 exports.list = function(req, res) {
 	var ref = new Firebase("https://meetsellerdb.firebaseio.com/");
 	ref.child("url").on("child_added", function(snapshot) {
-		console.log(snapshot.val());
+//		console.log(snapshot.val());
+		res.json({
+			list : snapshot.val()
+		});
 	}, function(errorObject) {
 		console.log("The read failed: " + errorObject.code);
 	});
